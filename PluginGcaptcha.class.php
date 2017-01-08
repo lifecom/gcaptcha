8179738970ef73d16a244f999d27d3a1ad1d8460
Stanislav Nevolin <stanislav@nevolin.info>
2017-01-08 23:56:59 +0500

    Adding main class

---
+++ PluginGcaptcha.class.php
@@ -0,0 +1,19 @@
+<?php
+/* 
+ * Plugin gCaptcha
+ * Author: Stanislav Nevolin, stanislav@nevolin.info
+ */
+
+class PluginGcaptcha extends Plugin {
+	public function Activate() {
+		return true;
+	}
+	
+	public function Deactivate() {
+		return true;
+	}
+	
+	public function Init() {
+		
+	}
+}
