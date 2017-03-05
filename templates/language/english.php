b3a09314df629e35c1f8a51f741e5b1ed40432dc
Stanislav Nevolin <stanislav@nevolin.info>
2017-03-06 02:48:41 +0500

    Добавили переводы

---
+++ templates/language/english.php
@@ -0,0 +1,9 @@
+<?php
+/* 
+ * Plugin InvisibleCaptcha
+ * Author: Stanislav Nevolin, stanislav@nevolin.info
+ */
+return array(
+	'must_set_sitekey' => 'You should fill out sitekey field in plugin configuration before activation',
+	'must_set_secretkey' => 'You should fill out secretkey field in plugin configuration before activation',
+);
---
+++ templates/language/russian.php
@@ -0,0 +1,9 @@
+<?php
+/* 
+ * Plugin InvisibleCaptcha
+ * Author: Stanislav Nevolin, stanislav@nevolin.info
+ */
+return array(
+	'must_set_sitekey' => 'Перед активацией необходимо заполнить sitekey в конфиге плагина',
+	'must_set_secretkey' => 'Перед активацией необходимо заполнить secretkey в конфиге плагина',
+);
